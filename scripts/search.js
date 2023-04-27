19577a2d7e158e394c8df1f3901dde2647bf4532
Jesus Cuadro <jesuscuadro190@gmail.com>
2023-04-27 15:32:30 -0500

    V.2 Version final

---
+++ scripts/search.js
@@ -0,0 +1,15 @@
+document.addEventListener("keyup", e=>{
+
+    if(e.key === "Escape")e.target.value=""
+
+    // e.target.matches("search")
+    // console.log(e.target.value);
+
+    if(e.target.matches("#search")){
+        document.querySelectorAll(".termino").forEach(palabra =>{
+            palabra.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+                ?palabra.classList.remove("filtro")
+                :palabra.classList.add("filtro")
+        })      
+    }
+})
